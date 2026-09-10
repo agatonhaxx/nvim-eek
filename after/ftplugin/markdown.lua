0f05836f261178070bce46d1df2e2e83173f0842
Agatonhaxx <glenn@huxe.eu>
2026-09-10 12:46:24 +0000

    added descriptions to zk keymaps in ftplugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -48,29 +48,21 @@ if require("zk.util").notebook_root(vim.fn.expand("%:p")) ~= nil then
 	end
 
 	map("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", "Follow Link")
-
-	-- Create a new note after asking for its title.
-	-- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
-	map("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", "")
-	-- Create a new note in the same directory as the current buffer, using the current selection for title.
-	map("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", "")
-	-- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
+	map("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", "ZkNew")
+	map(
+		"v",
+		"<leader>znt",
+		":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>",
+		"ZkNewFromTitleSelection"
+	)
 	map(
 		"v",
 		"<leader>znc",
 		":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
-		""
+		"ZkNewFromContentSelection"
 	)
-
-	-- Open notes linking to the current buffer.
-	map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", "")
-	-- Alternative for backlinks using pure LSP and showing the source context.
-	--map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', '')
-	-- Open notes linked by the current buffer.
-	map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", "")
-
-	-- Preview a linked note.
-	map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", "")
-	-- Open the code actions for a visual selection.
-	map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", "")
+	map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", "ZkBacklinks")
+	map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", "ZkLinks")
+	map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", "Preview Link")
+	map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", "Zk Code Action")
 end
